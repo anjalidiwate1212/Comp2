2d2c89ef894e842b62949fd2d32f8d5cfa733a48
prayagsingh6 <prayagsingh200@gmail.com>
2025-03-25 11:22:35 -0400

    prayag_commit

---
+++ library.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    public class Library
+    {
+        public List<Book> Books { get; private set; }
+        public List<Person> Patrons { get; private set; }
+
+        public Library()
+        {
+            Books = new List<Book>();
+            Patrons = new List<Person>();
+        }
+
+        public void AddBook(Book book)
+        {
+            Books.Add(book);
+        }
+
+        public void AddPatron(Person patron)
+        {
+            Patrons.Add(patron);
+        }
+
+        public void DisplayBooks()
+        {
+            Console.WriteLine("\nBooks in Library:");
+            foreach (var book in Books)
+            {
+                book.DisplayBookInfo();
+            }
+        }
+
+        public void DisplayPatrons()
+        {
+            Console.WriteLine("\nPatrons in Library:");
+            foreach (var patron in Patrons)
+            {
+                patron.DisplayInfo();
+            }
+        }
+
+        public bool BorrowBook(string bookTitle, string patronName)
+        {
+            Book book = Books.Find(b => b.Title == bookTitle);
+            if (book != null && book.BorrowBook())
+            {
+                Console.WriteLine($"{patronName} borrowed '{bookTitle}'");
+                return true;
+            }
+            Console.WriteLine($"{bookTitle} is not available for borrowing.");
+            return false;
+        }
+    }
+}
--- obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3238de8d1856ec99479a81f5f2cc62d899ccf861")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8718adb59c3ce8c76881f09de42d348062c97f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
